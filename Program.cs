c590751ae744477ac85bfa295c95556969636264
mark <markram0100@gmail.com>
2022-10-20 11:22:37 -0700

    done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// mark ramirez 
 // 10/19/2022 
 // mini challenge 7# reverse it
+// it will ask you for a number ir a word and then give that word or number back words
 
 Console.Clear();
 bool playing = true;
